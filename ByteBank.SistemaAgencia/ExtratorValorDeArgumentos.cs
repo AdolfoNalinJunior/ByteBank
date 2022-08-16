51512f28c023be25a18860c1d7cbe9dae763aa08
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-08-15 21:31:17 -0300

    Nestas duas aulas o professor mostra os métodos IsNullorEmpty que é uma exceção que vai retornar true quando o valor for nulo ou vazio.
    Também foi mostrado o médoto IndexOf que mostrar o indice do argumento que foi passado.
    
    A palavra Length é para saber quandos letras em uma palavra
    
     P6/m2/a1 e2

---
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentos
+    {
+        private readonly string _argumentos;
+        public string URL { get;}
+        public ExtratorValorDeArgumentos(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento não pode ser nulo ou vazio! ", nameof(url));
+            }
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            this.URL = url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            return "";
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -13,10 +13,28 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+           
             string url = "paginas?argumentos";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
             Console.WriteLine(url);
-            string argumento = url.Substring(8);
+            string argumento = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumento);
+
+            string vazia = "";
+            Console.WriteLine(String.IsNullOrEmpty(vazia));
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length); 
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+
         }
     }
 }
