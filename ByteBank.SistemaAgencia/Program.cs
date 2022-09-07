0b01150c9bafb575de292d4bbb5d134d4e33c238
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-09-07 09:44:41 -0300

    Estruturação do for para percorrer o array idades para ser ter a media e propriedade Length

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -9,6 +9,8 @@ using Humanizer;
 using System.Text.RegularExpressions;
 using ByteBank.Modelos.Funcionarios;
 using System.Net.Http.Headers;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -18,8 +20,7 @@ namespace ByteBank.SistemaAgencia
         {
             // Array de inteiros, com 5 posições
 
-            int[] idades = null;
-            //new int[5];
+            int[] idades = new int[5];
 
             idades[0] = 28;
             idades[1] = 15;
@@ -27,7 +28,21 @@ namespace ByteBank.SistemaAgencia
             idades[3] = 35;
             idades[4] = 50;
 
-            Console.WriteLine(idades[4]);
+            int acumulador = 0;
+
+            for (int indeci = 0; indeci < idades.Length; indeci++)
+            {
+                int idade = idades[indeci];
+
+                Console.WriteLine($"Acessando o array idade no índice {indeci}");
+                Console.WriteLine($"Valor de idade é [{indeci}] = {idade}");
+
+                acumulador += idade;
+            }
+            
+            int media = acumulador / idades.Length;
+
+            Console.WriteLine($"A media é {media}");
         } 
     }   
 }
