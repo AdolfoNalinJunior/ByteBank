74e83e85c7cb3bbddea4659f33d7446223335cd7
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-09-06 22:19:10 -0300

    Primeira aula da parte 7 C#. Como se construir um Array

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Humanizer;
 using System.Text.RegularExpressions;
 using ByteBank.Modelos.Funcionarios;
+using System.Net.Http.Headers;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -15,59 +16,17 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!");
-            Console.WriteLine(123);
-            Console.WriteLine(10.5);
-            Console.WriteLine(true);
+            // Array de inteiros, com 5 posições
 
-            Object conta = new ContaCorrente(123,34232);
-            Desenvolvedor desenvolvedor = new Desenvolvedor("siohfvsuio");
+            int[] idades = new int[5];
 
-            string contaToString = conta.ToString();
+            //idades[0] = 28;
+            //idades[1] = 15;
+            //idades[2] = 28;
+            //idades[3] = 35;
+            //idades[4] = 50;
 
-            Console.WriteLine("Resultado: " + contaToString);
-            Console.WriteLine(conta);
-
-            Cliente carlos_1 = new Cliente();
-            carlos_1.Nome = "Carlos";
-            carlos_1.CPF = "493.345.768-02";
-            carlos_1.Profissao = "Designer";
-
-            Cliente carlos_2 = new Cliente();
-            carlos_2.Nome = "Carlos";
-            carlos_2.CPF = "493.345.768-02";
-            carlos_2.Profissao = "Designer";
-
-            if (carlos_1.Equals(conta))
-            {
-                Console.WriteLine("São iguais");
-            }
-            else
-            {
-                Console.WriteLine("Não são iguais");
-            }
-
-        }
-
-        static void TestandoStrings()
-        {
-            // [0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]
-            // "[0-9]{4,5}[-][0-9]{4}"; 
-            // "[0-9]{4,5}[-]{0,1}[0-9]{4}"; 
-            // "[0-9]{4,5}-{0,1}[0-9]{4}"; 
-            string padrao = "[0-9]{4,5}-?[0-9]{4}";
-            // As chaves faz a quantificação, há quantidade de quantificação vai depender do número que esta dentos
-            // A variável padrao é uma "Expressão Regular" que mostra ou defini um padrao
-
-            string textoTeste = "Meu nome é Adolfo, me ligue em 99784-4546";
-            Match resultado = Regex.Match(textoTeste, padrao);
-            Console.WriteLine(resultado);
-
-            /*A classe Regex.IsMatch() retorna o valor verdadeiro ou falso, ou seja, se possui ou não esse padrão
-             
-            A classe Regex.Match() retorna valores de objetos, porem é só converter esse valor para string ou armazenar em 
-            uma variavel tipo Match, como é feita na variável resultado*/
-
-        }
-    }
+            Console.WriteLine(idades[4]);
+        } 
+    }   
 }
